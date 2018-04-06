88c207f7941ee08e52db5ad6e048b355cac9aeb6
John Wang <john@dashbase.io>
2018-04-06 12:12:44 -0700

    area

---
+++ geometry/area.py
@@ -0,0 +1,6 @@
+
+print ("this program will find area of a square")
+value = input("Please enter side of a square: ")
+side = float(value)
+area = side * side
+print ("the area of the square is: %.2f" % area)
